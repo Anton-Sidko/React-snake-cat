0ec74751fbea29a4434624ab28d56e2fcf50f2d6
Anton Sidko <sidko.antonanatoljevich@gmail.com>
2023-02-15 10:33:12 +0200

    Init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Snake cat 🐱</h1>
+      <h2>Test title</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+        itaque dolores dicta hic saepe ducimus impedit quas, nobis placeat
+        necessitatibus magni. Molestiae esse repudiandae doloribus quos porro,
+        labore incidunt qui.
+      </p>
+      <span>test text</span>
     </div>
   );
 }
 
 export default App;
+
