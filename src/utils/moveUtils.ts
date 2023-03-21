c0144a70d6e2396e8871e20f67b2089835d0ad8a
Anton Sidko <sidko.antonanatoljevich@gmail.com>
2023-03-21 15:23:06 +0200

    Fixed bug with none increment game score(due state mutual

--- src/utils/moveUtils.ts
+++ src/utils/moveUtils.ts
@@ -1,5 +1,11 @@
 import { SCORE_INCREMENT } from '../models/constants';
-import { CellType, GameState, GameStatus, SnakeSegment } from '../models/types';
+import {
+  CellType,
+  Food,
+  GameState,
+  GameStatus,
+  SnakeSegment,
+} from '../models/types';
 import { buildGrid, getNextCell } from './gridUtils';
 
 const moveSnake = function (state: GameState): SnakeSegment | null {
@@ -35,40 +41,24 @@ const checkFoodEated = function (
   state: GameState,
   oldSnakeHead: SnakeSegment,
   snakeHead: SnakeSegment
-): number {
+): [number, Food[]] {
   const { food } = state;
   const { row, col } = snakeHead;
   const foodIndex = food.findIndex(f => f.row === row && f.col === col);
-  let foodEated = 0;
-  // let segment: SnakeSegment | undefined = snakeHead;
 
-  console.log({ foodIndex });
+  let newGamePoints = 0;
+  let newFood = [...food];
 
-  if (foodIndex > 0) {
-    console.log({ foodIndex });
-    foodEated++;
-    food.splice(foodIndex, 1);
-    console.log('eat');
-    // return 10;
+  if (foodIndex !== -1) {
+    newGamePoints = SCORE_INCREMENT;
+    newFood = [...food.slice(0, foodIndex), ...food.slice(foodIndex + 1)];
   }
-  // while (segment) {
-  //   const { row, col } = segment;
-  //   const index = food.findIndex(veg => {
-  //     return veg.row === row && veg.col === col;
-  //   });
-  //   if (index !== -1) {
-  //     foodEated++;
-  //     food.splice(index, 1);
-  //   }
-  //   // tail = segment;
-  //   segment = segment.next;
-  // }
 
-  if (foodEated === 0) {
-    console.log('toys');
-    return 0;
+  if (newGamePoints === 0) {
+    return [0, newFood];
   }
-  return 1;
+
+  return [newGamePoints, newFood];
 };
 
 export const move = function (state: GameState): GameState {
@@ -79,29 +69,13 @@ export const move = function (state: GameState): GameState {
     return { ...state, gameStatus: GameStatus.FINISHED };
   }
 
-  const foo0dEated = checkFoodEated(state, snakeHead, newSnakeHead);
-  // console.log({
-  //   // checkFoodEated: checkFoodEated(state, snakeHead, newSnakeHead),
-  //   foo0dEated,
-  // });
-
-  let newGamePoints = 0;
-  if (foo0dEated) {
-    newGamePoints = gamePoints + SCORE_INCREMENT * 1;
-  }
-
-  // console.log({ newGamePoints });
-
-  // console.log(
-  //   { gamePoints: gamePoints + SCORE_INCREMENT * foodEated },
-  //   { gamePoints },
-  //   { newGamePoints }
-  // );
+  const [scoreInc, newFood] = checkFoodEated(state, snakeHead, newSnakeHead);
 
   return {
     ...state,
     snakeHead: newSnakeHead,
-    gamePoints: newGamePoints,
-    grid: buildGrid(fieldSize, newSnakeHead, food),
+    gamePoints: gamePoints + scoreInc,
+    food: newFood,
+    grid: buildGrid(fieldSize, newSnakeHead, newFood),
   };
 };
