c0d53e266fce598a99b9f919f305f32c686c5844
Anton Sidko <sidko.antonanatoljevich@gmail.com>
2023-03-23 11:19:34 +0200

    Deleted component to solve Netlify build problem

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 
 import { GameContextProvider } from './context/GameContext';
 
-import Header from './components/header/header.component';
+import Header from './components/header/Header.component';
 import SettingsBar from './components/settings-bar/SettingsBar.component';
 import StatusBar from './components/status-bar/StatusBar.component';
 import Grid from './components/grid/Grid.component';
