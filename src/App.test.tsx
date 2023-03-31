1d46440d59c12f21bec58b1a070f3cf47b9275cc
Anton Sidko <sidko.antonanatoljevich@gmail.com>
2023-03-31 11:02:20 +0300

    Added some tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders game name in Header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const gameName = screen.getByText(/Snake cat/i);
+  expect(gameName).toBeInTheDocument();
 });
+
+test('start point equal to zero', () => {
+  render(<App />);
+  const startPoint = screen.getByText(/Current point/i);
+  expect(startPoint).toBeInTheDocument();
+  expect(startPoint.textContent?.slice(-1)).toEqual('0');
+});
+
+test('wall disabled by default', () => {
+  render(<App />);
+  const wallCheckBox = screen.getByLabelText('Wall');
+  expect(wallCheckBox).toBeInTheDocument();
+  expect(wallCheckBox).not.toBeChecked();
+});
+
